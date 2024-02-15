e96efc6767a6e7884f94457d57ce71df1d42a419
RY2291 <prgr2498@gmail.com>
2024-02-15 23:04:32 +0900

    htmlのエスケープ処理

--- php/libs/helper.php
+++ php/libs/helper.php
@@ -1,32 +1,59 @@
-<?php 
+<?php
 
-function getParam($key, $default_val, $is_post = true){
-  $arry = $is_post ? $_POST : $_GET;
-  // var_export($arry);
-  return $arry[$key] ?? $default_val;
+function getParam($key, $default_val, $is_post = true)
+{
+	$arry = $is_post ? $_POST : $_GET;
+	// var_export($arry);
+	return $arry[$key] ?? $default_val;
 }
 
-function redirect($path){
-  echo $path;
-  if($path === GO_HOME){
-    $path = 'home';
-  } else if($path === GO_REFERER){
-    $path = $_SERVER['HTTP_REFERER'];
-  } else {
-    $path = getUrl($path);
-  }
-  header("Location: {$path}");
-  die();
+function redirect($path)
+{
+	echo $path;
+	if ($path === GO_HOME) {
+		$path = 'home';
+	} else if ($path === GO_REFERER) {
+		$path = $_SERVER['HTTP_REFERER'];
+	} else {
+		$path = getUrl($path);
+	}
+	header("Location: {$path}");
+	die();
 }
 
-function getUrl($path){
-  return BASE_URL . trim($path, '/');
+function getUrl($path)
+{
+	return BASE_URL . trim($path, '/');
 }
 
-function theUrl($path){
-  echo getUrl($path);
+function theUrl($path)
+{
+	echo getUrl($path);
 }
 
-function is_alnum($val){
-  return preg_match("/^[a-zA-Z0-9]+$/",$val);
+function is_alnum($val)
+{
+	return preg_match("/^[a-zA-Z0-9]+$/", $val);
+}
+
+function escape($data)
+{
+	if (is_array($data)) {
+
+		foreach ($data as $prop => $val) {
+			$data[$prop] = escape($val);
+		}
+
+		return $data;
+	} elseif (is_object($data)) {
+
+		foreach ($data as $prop => $val) {
+			$data->$prop = escape($val);
+		}
+
+		return $data;
+	} else {
+
+		htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
+	}
 }
